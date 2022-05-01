5ff76b610cc41156fe40f51aa0234e36121d9258
Ori Azualy <ori1019@hotmail.com>
2022-05-01 21:21:01 +0300

    final design for the chat app - with adadding style to the chat list, adding validation for register page.

---
+++ Valid.js
@@ -0,0 +1,13 @@
+function isPositiveInteger(str) {
+    if (typeof str !== 'string') {
+      return false;
+    }
+  
+    const num = Number(str);
+  
+    if (Number.isInteger(num) && num > 0) {
+      return true;
+    }
+  
+    return false;
+}
--- loginScript.js
+++
@@ -1,5 +0,0 @@
-
-
-function loginFunc(){
-    window.alert("here?");
-}
